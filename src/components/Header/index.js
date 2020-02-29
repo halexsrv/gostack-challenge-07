df03ab793d395e16289c42dcac11904938e9adb7
Alex Vitorino <halexsrv@gmail.com>
2020-02-29 14:56:07 -0300

    add count to cart list on header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { HeaderContainer, Container, HeaderItem, Logo } from './styles';
+import {
+  HeaderContainer,
+  Container,
+  HeaderItem,
+  Logo,
+  ListCount,
+  ListCountText,
+} from './styles';
 
 import logo from '../../assets/logo.png';
 
@@ -13,9 +20,12 @@ export default function Header({ navigation }) {
         <HeaderItem onPress={() => navigation.navigate('Home')}>
           <Logo source={logo} />
         </HeaderItem>
-        <HeaderItem onPress={() => navigation.navigate('Cart')}>
-          <Icon name="shopping-basket" size={24} color="#fff" />
-        </HeaderItem>
+        <ListCount>
+          <HeaderItem onPress={() => navigation.navigate('Cart')}>
+            <Icon name="shopping-basket" size={24} color="#fff" />
+          </HeaderItem>
+          <ListCountText>3</ListCountText>
+        </ListCount>
       </Container>
     </HeaderContainer>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,6 +4,8 @@ import colors from '../../styles/colors';
 
 export const HeaderContainer = styled.SafeAreaView`
   background: ${colors.dark};
+  /* flex-direction: row; */
+  flex: 0;
 `;
 
 export const Container = styled.View`
@@ -20,4 +22,25 @@ export const HeaderItem = styled.TouchableOpacity`
 export const Logo = styled.Image`
   width: 185px;
   height: 24px;
+`;
+
+export const ListCount = styled.View`
+  flex: 1;
+  align-items: flex-end;
+  justify-content: flex-end;
+`;
+
+export const ListCountText = styled.Text`
+  position: absolute;
+  overflow: hidden;
+  top: 10px;
+  right: 10px;
+  padding: 2px;
+  font-size: 12px;
+  border-radius: 10px;
+  text-align: center;
+  background: ${colors.primary};
+  color: #fff;
+  height: 20px;
+  width: 20px;
 `;
