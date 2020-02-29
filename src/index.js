1d1175c3d7859021a5135f457fd12274aef69811
Alex Vitorino <halexsrv@gmail.com>
2020-02-29 15:11:44 -0300

    add config for redux and redux-saga to store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
@@ -6,12 +7,13 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 
 import colors from './styles/colors';
+import store from './store';
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor={colors.dark} />
       <Routes />
-    </>
+    </Provider>
   );
 }
