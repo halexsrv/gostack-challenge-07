290603c1b64cddbf82f5584ad3fe927bb9ebfdad
Alex Vitorino <halexsrv@gmail.com>
2020-02-29 21:16:18 -0300

    add setup for cart page with list of items order

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,12 +1,126 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, FlatList, View, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { formatPrice } from '../../util/format';
 
-export default function Cart() {
+import * as CartActions from '../../store/modules/cart/action';
+
+import colors from '../../styles/colors';
+
+import {
+  Container,
+  ProductList,
+  ProductItem,
+  ProductContainer,
+  ProductItemImage,
+  ProductItemDetails,
+  ProductItemTitle,
+  ProductItemPrice,
+  ProductItemRemove,
+  EmptyList,
+  EmptyListText,
+  ProductItemTotal,
+  ProductItemCount,
+  ProductItemQuantity,
+  ProductItemQuantityText,
+  ProductItemTotalText,
+  Total,
+  TotalText,
+  TotalPriceText,
+  FinishOrder,
+  FinishOrderText,
+} from './styles';
+
+function Cart({ cart, total, cartSize, removeFromCart, updateAmountRequest }) {
+  function increment(product) {
+    updateAmountRequest(product.id, product.amount + 1);
+  }
+
+  function decrement(product) {
+    updateAmountRequest(product.id, product.amount - 1);
+  }
+
+  if (cartSize === 0) {
+    return (
+      <Container>
+        <EmptyList>
+          <Icon name="remove-shopping-cart" size={64} color="#999" />
+          <EmptyListText>Seu Carrinho está vazio.</EmptyListText>
+        </EmptyList>
+      </Container>
+    );
+  }
+
   return (
     <Container>
-      <Text style={{ color: '#fff' }}>Cart Page!</Text>
+      <ProductList>
+        {cart.map(item => (
+          <ProductItem key={item.id}>
+            <ProductContainer>
+              <ProductItemImage source={{ uri: item.image }} />
+              <ProductItemDetails>
+                <ProductItemTitle>{item.title}</ProductItemTitle>
+                <ProductItemPrice>{item.priceFormatted}</ProductItemPrice>
+              </ProductItemDetails>
+              <ProductItemRemove onPress={() => removeFromCart(item.id)}>
+                <Icon name="delete-forever" size={24} color={colors.primary} />
+              </ProductItemRemove>
+            </ProductContainer>
+            <ProductItemTotal>
+              <ProductItemCount>
+                <TouchableOpacity onPress={() => decrement(item)}>
+                  <Icon
+                    name="remove-circle-outline"
+                    size={24}
+                    color={colors.primary}
+                  />
+                </TouchableOpacity>
+                <ProductItemQuantity>
+                  <ProductItemQuantityText>
+                    {item.amount}
+                  </ProductItemQuantityText>
+                </ProductItemQuantity>
+                <TouchableOpacity onPress={() => increment(item)}>
+                  <Icon
+                    name="add-circle-outline"
+                    size={24}
+                    color={colors.primary}
+                  />
+                </TouchableOpacity>
+              </ProductItemCount>
+              <ProductItemTotalText>{item.subTotal}</ProductItemTotalText>
+            </ProductItemTotal>
+          </ProductItem>
+        ))}
+        <Total>
+          <TotalText>TOTAL</TotalText>
+          <TotalPriceText>{total}</TotalPriceText>
+        </Total>
+        <FinishOrder>
+          <FinishOrderText>FINALIZAR PEDIDO</FinishOrderText>
+        </FinishOrder>
+      </ProductList>
     </Container>
   );
 }
+
+const mapStateToProps = state => ({
+  cart: state.cart.map(product => ({
+    ...product,
+    subTotal: formatPrice(product.price * product.amount),
+  })),
+  cartSize: state.cart.length,
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,6 +1,126 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  flex: 1;
+import colors from '../../styles/colors';
+
+export const Container = styled.View``;
+
+export const ProductList = styled.View`
   margin: 20px;
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+`;
+
+export const ProductItem = styled.View``;
+
+export const ProductContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ProductItemImage = styled.Image`
+  width: 80px;
+  height: 80px;
+`;
+
+export const ProductItemDetails = styled.View`
+  flex: 1;
+  /* width: 200px; */
+  height: 80px;
+  margin: 10px;
+  justify-content: space-between;
+`;
+
+export const ProductItemTitle = styled.Text`
+  font-size: 14px;
+  color: #333;
+`;
+
+export const ProductItemPrice = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const ProductItemRemove = styled.TouchableOpacity`
+  padding: 4px;
+`;
+
+export const EmptyList = styled.View`
+  background: #fff;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  margin: 20px;
+  border-radius: 4px;
+`;
+
+export const EmptyListText = styled.Text`
+  margin-top: 20px;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const ProductItemTotal = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background: #eee;
+  margin-top: 10px;
+  padding: 10px;
+  border-radius: 4px;
+`;
+
+export const ProductItemCount = styled.View`
+  flex-direction: row;
+`;
+
+export const ProductItemQuantity = styled.View`
+  background: #fff;
+  margin: 0 5px;
+  width: 40px;
+  padding: 4px;
+  align-items: center;
+  border-radius: 4px;
+`;
+
+export const ProductItemQuantityText = styled.Text`
+  font-size: 14px;
+  color: #666;
+`;
+
+export const ProductItemTotalText = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const Total = styled.View`
+  margin: 40px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TotalText = styled.Text`
+  font-size: 16px;
+  color: #999;
+  font-weight: bold;
+`;
+
+export const TotalPriceText = styled.Text`
+  margin-top: 20px;
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+export const FinishOrder = styled.View`
+  background: ${colors.primary};
+  border-radius: 4px;
+  align-items: center;
+`;
+
+export const FinishOrderText = styled.Text`
+  margin: 10px;
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
 `;
